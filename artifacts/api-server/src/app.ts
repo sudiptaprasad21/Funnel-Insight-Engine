296a726c78be811431c271fba9e87d80c1469990
sudiptaprasad21 <sudiptaprasad21@gmail.com>
2026-05-18 21:17:32 +0530

    Bypasses Vercel TypeScript incompatibility

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,5 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
+// @ts-ignore
 import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
